46c3a8926823a76f129d3b5d7f50db63dc316b07
jeremykuang <jeristiano@users.noreply.github.com>
2017-11-11 11:18:37 +0800

    Update index.php

--- PHP/errorHandle/index.php
+++ PHP/errorHandle/index.php
@@ -1,5 +1,6 @@
 <?php
 /**
+* 在根文件引入类和注册方法
  * Created by PhpStorm.
  * User: JeremyK
  * Date: 2017/11/02
@@ -9,4 +10,3 @@ include "Buffon/RegisterErrorHandler.class.php";
 define('DEBUG_ERROR',0);
 define('ROOT_PATH',dirname(__FILE__));
 RegisterErrorHandler::register();
-RegisterErrorHandler::all();
